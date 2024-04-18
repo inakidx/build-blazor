c2ef114b50e0a3c709c6fe7233eb80aa4f86f90d
inakidx <inakidx@gmail.com>
2024-04-19 00:13:21 +0200

    Update work.sh

--- lib/work.sh
+++ lib/work.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-while true; do
-    # Coloca aquí el comando que deseas ejecutar
-    echo "Ejecutando tarea..."
-    # Espera 1 minuto antes de la próxima ejecución
-    sleep 60
+# Tiempo de ejecución en segundos (5 minutos = 300 segundos)
+tiempo_total=300
+
+# Tiempo de espera entre iteraciones del bucle en segundos
+intervalo=1
+
+# Calcula el número de iteraciones del bucle en función del tiempo total y el intervalo
+iteraciones=$((tiempo_total / intervalo))
+
+# Bucle while que se ejecuta durante el tiempo especificado
+contador=0
+while [ $contador -lt $iteraciones ]; do
+    echo "Ejecutando bucle... iteración $((contador + 1))"
+    # Coloca aquí el código que deseas ejecutar en cada iteración
+
+    # Incrementa el contador
+    contador=$((contador + 1))
+    # Espera el intervalo de tiempo especificado antes de la siguiente iteración
+    sleep $intervalo
 done
+
+echo "Bucle completado."
