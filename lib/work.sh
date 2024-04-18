49e90828ce11f88a765bfafba4db9b6f6d5d0148
inakidx <inakidx@gmail.com>
2024-04-19 00:10:37 +0200

    Create work.sh

---
+++ lib/work.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while true; do
+    # Coloca aquí el comando que deseas ejecutar
+    echo "Ejecutando tarea..."
+    # Espera 1 minuto antes de la próxima ejecución
+    sleep 60
+done
